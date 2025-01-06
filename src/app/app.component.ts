73a31d0105cd7aad46952abbfb2dbf47ed82d403
Anja Gollner <anjakgollner@gmail.com>
2025-01-06 15:05:29 +0400

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './shared/footer/footer.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, FooterComponent, NavbarComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  title = 'portfolio';
+}
---
+++ src/app/main-content/main-content.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { HeroComponent } from './hero/hero.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ReferencesComponent } from './references/references.component';
+import { ContactComponent } from './contact/contact.component';
+
+@Component({
+  selector: 'app-main-content',
+  standalone: true,
+  imports: [
+    HeroComponent,
+    AboutMeComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    ReferencesComponent,
+    ContactComponent,
+  ],
+  templateUrl: './main-content.component.html',
+  styleUrl: './main-content.component.scss',
+})
+export class MainContentComponent {}
