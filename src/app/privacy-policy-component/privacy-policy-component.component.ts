be451ccdb29db3faa65e97b4d7609b911d93b001
Anja Gollner <anjakgollner@gmail.com>
2025-01-09 19:59:20 +0400

    legal notice

---
+++ src/app/privacy-policy-component/privacy-policy-component.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-privacy-policy-component',
+  standalone: true,
+  imports: [TranslateModule],
+  templateUrl: './privacy-policy-component.component.html',
+  styleUrl: './privacy-policy-component.component.scss'
+})
+export class PrivacyPolicyComponentComponent {
+
+}
